1c3b8b54df53b24104b2c9eb26b51a18ada8f359
Abhay <91288859+A89881@users.noreply.github.com>
2026-01-30 23:44:09 +0100

    Accomplished FInal Module 4
    Did Question 4b:
    
    It was plotting the functions we calculated into the heat map!
    
    Confusing instructions but much learned!

--- roadster/script_part4b.py
+++ roadster/script_part4b.py
@@ -18,4 +18,24 @@ plt.xlabel('Time [hour of day]',fontsize=18)
 plt.ylabel('Distance [km]',fontsize=18)
 plt.title('Speed [km/h]',fontsize=18)
 fig.colorbar(cs)
+plt.savefig("speed-data-nyc.eps", bbox_inches='tight')
+
+# Add Euler route travelers for two different start times - Using nyc_route_traveler_euler
+# Arbitrary step size for Euler method
+h_euler = 0.1  # in hours
+
+# Trip 1: start 04:00 -> t0 = 4.0
+t0_a = 4.0
+time_a, dist_a, speed_a = route_nyc.nyc_route_traveler_euler(t0_a, h_euler)
+plt.plot(time_a, dist_a, linewidth=1, marker='s', label='Trip start 04:00 (Euler)')
+
+# Trip 2: start 09:30 -> t0 = 9.5
+t0_b = 9.5
+time_b, dist_b, speed_b = route_nyc.nyc_route_traveler_euler(t0_b, h_euler)
+plt.plot(time_b, dist_b, linewidth=1, marker='o', label='Trip start 09:30 (Euler)')
+
+
+plt.savefig("speed-data-nyc.eps", bbox_inches='tight')
 plt.savefig("speed-data-nyc.png", bbox_inches='tight')
+plt.legend(loc='best', fontsize=12)
+plt.show()
